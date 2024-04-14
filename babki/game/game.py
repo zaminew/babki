c0682827bd417a38acd864bc7366842e0948a077
Eugene <zaminew@yandex.ru>
2024-04-14 18:26:57 -0400

    game changes

--- babki/game/game.py
+++ babki/game/game.py
@@ -6,7 +6,33 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Формируем путь к файлу 'doodads.json'
-eventsfilePath = os.path.join(current_dir, 'events.json')
+events_file_Path = os.path.join(current_dir, 'data/events.json')
+
+class Player:
+    def __init__(self, name, balance, salary_level):
+        self.name = name
+        self.balance = balance
+        self.salary_level = salary_level
+        self.credits = 0
+        self.child = False
+
+    def take_credit(self, amount):
+        pass
+
+    def repay_credit(self, amount):
+        pass
+
+    def calculate_balance(self):
+        pass
+
+
+class Action:
+    def __init__(self, buy=False, buy_amount=0, sell=False, sell_amount=0, check=False):
+        self.buy = buy
+        self.buy_amount = buy_amount
+        self.sell = sell
+        self.sell_amount = sell_amount
+        self.check = check
 
 class JsonLoader:
     @staticmethod
@@ -18,10 +44,14 @@ class Event:
     def __init__(self, name, effect):
         self.name = name
         self.effect = effect
-        self.inputRequires = False
+        self.input_requires = False
 
-    def apply_effect(self, player):
-        pass
+    def get_action(self, player : Player) -> Action:
+        return Action(False, 0, False, 0, True)
+
+    def execute_action(self, player : Player, action : Action) -> bool:
+        return True
+
         #TODO наследники в результате должны вывести доступные действие с данным событием 
             # а пользователь может применить это действие на событии и получить результат
             # возможно понадобится несколько функций, вывод, ввод, результат. И это для одного шага
@@ -30,53 +60,48 @@ class Event:
 class ExpenseEvent(Event):
     def __init__(self, name, effect, expenses_data):
         super().__init__(name, effect)
-        self.expenses_data = expenses_data
+        self.expense = random.choice(expenses_data["doodads"])
 
-    def apply_effect(self, player):
-        expense = random.choice(self.expenses_data["doodads"])
-        player.balance -= expense["cost"]
-        print(f"{self.name}: {expense['title']} -{expense['cost']}")
+    def get_action(self, player : Player) -> Action:
+        return Action(False, 0, False, 0, True)
+
+    def execute_action(self, player, action : Player):
+        cost = self.expense['cost']
+        if player.balance >= cost:
+            player.balance -= cost
+            return True
+        return False
+
+
 
 
 class StockEvent(Event):
     def __init__(self, name, effect, stocks_data):
         super().__init__(name, effect)
-        self.stocks_data = stocks_data
+        self.stock = random.choice(stocks_data["stocks"])
 
-    def apply_effect(self, player):
-        stock = random.choice(self.stocks_data["stocks"])
-        self.inputRequires = True
-        print(f"{self.name}: {stock['symbol']} цена за акцию сегодня ${stock['price']}")
-        
-        action = input("Хотите купить (buy) или продать (sell) акцию? ")
+    def get_action(self, player : Player) -> Action:
+        act = Action(False, 0, False, 0, True)
+        price = self.stock['price']
+        if price > player.balance:
+            act.buy = True
+            act.buy_amount = player.balance // price
+        # TODO if player has stock he can sell
+        return act
 
-        if action.lower() == "buy":
-            player.balance -= stock["price"]
-            print(f"Вы купили акцию {stock['symbol']} за ${stock['price']}")
-        elif action.lower() == "sell":
-            player.balance += stock["price"]
-            print(f"Вы продали акцию {stock['symbol']} за ${stock['price']}")
-        else:
-            print("Неверное действие. Попробуйте снова.")
-
-        print(f"Баланс: ${player.balance}")
-
-
-class Player:
-    def __init__(self, name, balance, salary_level):
-        self.name = name
-        self.balance = balance
-        self.salary_level = salary_level
-        self.credits = 0
-
-    def take_credit(self, amount):
-        pass
-
-    def repay_credit(self, amount):
-        pass
-
-    def calculate_balance(self):
-        pass
+    def execute_action(self, player : Player, action : Action) -> bool:
+        price = self.stock['price']
+        if action.buy:
+            summ = action.buy_amount * price
+            player.balance -= summ
+            print(f"{self.name}: Покупка -{summ}")
+        elif action.sell:
+            summ = action.sell_amount * price
+            player.balance += summ 
+            print(f"{self.name}: Продажа +{summ}")
+        elif action.check:
+            print(f"{self.name}: Проехали")
+        return True
 
 class Game:
     def __init__(self, player, events_data, expenses_data):
@@ -94,23 +119,35 @@ class Game:
 
     def play_month(self):
         event = random.choice(self.events_data)
-        event.apply_effect(self.player)
-        if event.inputRequires:
+        act = event.get_action(self.player)
+        print(f"name {event.name}")
+        if act.buy:
+            print(" BUY ")
+            print(act.buy_amount)
+        if act.sell:
+            print(" SELL ")
+            print(act.sell_amount)
+        if act.check:
+            print(" CHECK ")
+
+        
+        event.execute_action(self.player, Action(False, 0, False, 0, True))
+        if event.input_requires:
             print("")
         self.current_step += 1
         
         print(f"Step: {self.current_step}")
         if self.current_step % 4 == 0:
             print("PAYDAY")
-            player.balance += player.salary_level
+            self.player.balance += self.player.salary_level
         print(f"Balance: {self.player.balance}")
         
 
 # Загрузка данных из JSON
 events_data = [
     Event("Бизнес, недвижимость", "Купи продай большой и малый бизнес или недвижимость, а еще есть сетевые"),
-    ExpenseEvent("Непредвиденные расходы", "Произошел неожиданный расход", JsonLoader.load(eventsfilePath)),
-    StockEvent("Фондовый рынок", "Кпи продай акции облигации и другие инструменты", JsonLoader.load(eventsfilePath))
+    ExpenseEvent("Непредвиденные расходы", "Произошел неожиданный расход", JsonLoader.load(events_file_Path)),
+    StockEvent("Фондовый рынок", "Кпи продай акции облигации и другие инструменты", JsonLoader.load(events_file_Path))
 ]
 
 #TODO в самом событии будет указано что можно делать с этим событием и какие кнопки будут доступны а так же доступное колличество
@@ -118,7 +155,7 @@ events_data = [
 
 player = Player("Player1", 10000, 1000)
 
-game = Game(player, events_data, JsonLoader.load(eventsfilePath))
+game = Game(player, events_data, JsonLoader.load(events_file_Path))
 
 while True:
     game.play_month()

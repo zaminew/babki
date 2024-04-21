71f53fb3539562c450533c22dbed779b94234b7d
Eugene <zaminew@yandex.ru>
2024-04-20 20:41:05 -0400

    some changes

--- babki/game/action.py
+++ babki/game/action.py
@@ -1,18 +1,10 @@
 from enum import Enum
 
-class Action:
-    def __init__(self, buy=False, buy_amount=0, sell=False, sell_amount=0, check=False):
-        self.buy = buy
-        self.buy_amount = buy_amount
-        self.sell = sell
-        self.sell_amount = sell_amount
-        self.check = check
-
 class EnumStr(Enum):
     def __str__(self):
         return self.value
     
-class ActionNew(EnumStr):
+class Action(EnumStr):
     BUY = 'buy'
     SELL = 'sell'
     CHECK = 'check'
--- babki/game/strategies.py
+++ babki/game/strategies.py
@@ -1,62 +1,67 @@
 from player import Player
 from event import *
-from action import ActionNew
+from action import Action
 
 class ActionStrategy:
-    def execute(self, player, event):
-        return True, 'function not implemented'
+    def execute(self, player : Player, event : Event, amount : int):
+        return False, 'function not implemented'
 
 class BuyStrategy(ActionStrategy):
-    def execute(self, player, event):
+    def execute(self, player : Player, event : Event, amount : int):
         if isinstance(event, StockEvent):
-            # Логика для покупки акций
-            return True, 'buy stock function not implemented'
+            return True, f'Вы купили {event.name} в количестве {amount} по цене {event.price} на сумму {event.price*amount}'
         elif isinstance(event, PropertyEvent):
-            # Логика для покупки дома
-            return True, 'buy property function not implemented'
-
+            return False, 'buy Property function not implemented'
+        elif isinstance(event, ExpenseEvent):
+            return False, 'Вы не можете использовать покупку для этого события'
+        
 class SellStrategy(ActionStrategy):
-    def execute(self, player, event):
+    def execute(self, player : Player, event : Event, amount : int):
         if isinstance(event, StockEvent):
-            # Логика для продажи акций
-            return True, 'sell stock function not implemented'
+            return True, f'Вы продали {event.name} в количестве {amount} по цене {event.price} на сумму {event.price*amount}'
         elif isinstance(event, PropertyEvent):
-            # Логика для продажи дома
-            return True, 'sell property function not implemented'
-
+            return False, 'sell Property function not implemented'
+        elif isinstance(event, ExpenseEvent):
+            return False, 'Вы не можете использовать продажу для этого события'
+        
 class CheckStrategy(ActionStrategy):
-    def execute(self, player, event):
+    def execute(self, player : Player, event : Event, amount : int):
         if isinstance(event, StockEvent):
-            # Логика для продажи акций
-            return True, 'check stock function not implemented'
+            return True, 'Вы пропустили это событие'
         elif isinstance(event, PropertyEvent):
-            # Логика для продажи дома
-            return True, 'check property function not implemented'
-
+            return True, 'Вы пропустили это событие'
+        elif isinstance(event, ExpenseEvent):
+            if player.balance >= event.cost:
+                player.balance -= event.cost
+                return True, f'Вы потеряли {event.cost} на безделушки'
+            else:
+                return False, f'Вам не хватает {event.cost - player.balance} для покупки {event.name} за {event.cost}'
+        
 # Фабрика стратегий
 class StrategyFactory:
     @staticmethod
-    def get_strategy(action_type : ActionNew):
-        if action_type == ActionNew.BUY:
+    def get_strategy(action_type : Action):
+        if action_type == Action.BUY:
             return BuyStrategy()
-        elif action_type == ActionNew.SELL:
+        elif action_type == Action.SELL:
             return SellStrategy()
-        elif action_type == ActionNew.CHECK:
+        elif action_type == Action.CHECK:
             return CheckStrategy()
         else:
             return None
 
+
 # Пример использования
 player = Player('Eugene', 10000, 1000)
 
-# Получение действия и события
-action_type = "buy"  # Предположим, что это действие
-event_type = "stock"  # Предположим, что это тип события
-
 # Получение стратегии и ее выполнение
-strategy = StrategyFactory.get_strategy(ActionNew.BUY)
-if strategy:
-    res = strategy.execute(player, StockEvent('MySymbol', 'description', 777))
-    print(res)
+buy_strategy = StrategyFactory.get_strategy(Action.BUY)
+check_strategy = StrategyFactory.get_strategy(Action.CHECK)
+
+if buy_strategy and check_strategy:
+    buy_res = buy_strategy.execute(player, StockEvent('MySymbol', 'description', 777), 100)
+    check_res = check_strategy.execute(player, ExpenseEvent('Наушники для компа', 'старые уже облезли', 50000, 100, 10000, 100, 0), 100)
+    print(buy_res[1] + '\n' + check_res[1])
 else:
     print('unknown strategy')
+print(player.balance)
